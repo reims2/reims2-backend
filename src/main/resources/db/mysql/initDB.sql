3e30628068d597f780283c22d19d42bbf5bc1012
Thomas <substantialimpulse@pm.me>
2021-05-29 17:34:25 +0200

    Switch to mysql as database

--- src/main/resources/db/mysql/initDB.sql
+++ src/main/resources/db/mysql/initDB.sql
@@ -1,82 +1,49 @@
-CREATE DATABASE IF NOT EXISTS petclinic;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS eye;
+DROP TABLE IF EXISTS glasses;
+DROP TABLE IF EXISTS dispense;
 
-ALTER DATABASE petclinic
-  DEFAULT CHARACTER SET utf8
-  DEFAULT COLLATE utf8_general_ci;
+CREATE TABLE eye (
+  id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+  sphere VARCHAR(30),
+  cylinder VARCHAR(30),
+  axis VARCHAR(30),
+  additional  VARCHAR(30)
+);
 
-GRANT ALL PRIVILEGES ON petclinic.* TO pc@localhost IDENTIFIED BY 'pc';
+CREATE TABLE dispense (
+      id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+      modify_date TIMESTAMP NULL DEFAULT NULL
+);
+CREATE TABLE glasses (
+     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+     SKU BIGINT,
+     glasses_type VARCHAR(30),
+     glasses_size VARCHAR(30),
+     appearance  VARCHAR(30),
+     dispense_id   BIGINT,
+     location VARCHAR(50),
+     dispensed   BOOLEAN DEFAULT FALSE,
+     OS_ID BIGINT,
+     OD_ID BIGINT
+);
+ALTER TABLE glasses ADD CONSTRAINT fk_glasses_OS FOREIGN KEY (OS_ID) REFERENCES eye(id);
+ALTER TABLE glasses ADD CONSTRAINT fk_glasses_OD FOREIGN KEY (OD_ID) REFERENCES eye(id);
+ALTER TABLE glasses ADD CONSTRAINT fk_dispense FOREIGN KEY (dispense_id) REFERENCES dispense(id);
 
-USE petclinic;
-
-CREATE TABLE IF NOT EXISTS vets (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  INDEX(last_name)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS specialties (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(80),
-  INDEX(name)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS vet_specialties (
-  vet_id INT(4) UNSIGNED NOT NULL,
-  specialty_id INT(4) UNSIGNED NOT NULL,
-  FOREIGN KEY (vet_id) REFERENCES vets(id),
-  FOREIGN KEY (specialty_id) REFERENCES specialties(id),
-  UNIQUE (vet_id,specialty_id)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS types (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(80),
-  INDEX(name)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS owners (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  address VARCHAR(255),
-  city VARCHAR(80),
-  telephone VARCHAR(20),
-  INDEX(last_name)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS pets (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30),
-  birth_date DATE,
-  type_id INT(4) UNSIGNED NOT NULL,
-  owner_id INT(4) UNSIGNED NOT NULL,
-  INDEX(name),
-  FOREIGN KEY (owner_id) REFERENCES owners(id),
-  FOREIGN KEY (type_id) REFERENCES types(id)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS visits (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  pet_id INT(4) UNSIGNED NOT NULL,
-  visit_date DATE,
-  description VARCHAR(255),
-  FOREIGN KEY (pet_id) REFERENCES pets(id)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS users (
-  username VARCHAR(20) NOT NULL ,
-  password VARCHAR(20) NOT NULL ,
-  enabled TINYINT NOT NULL DEFAULT 1 ,
+CREATE  TABLE users (
+  username    VARCHAR(20) NOT NULL ,
+  password    VARCHAR(20) NOT NULL ,
+  enabled     BOOLEAN DEFAULT TRUE NOT NULL ,
   PRIMARY KEY (username)
-) engine=InnoDB;
+);
 
-CREATE TABLE IF NOT EXISTS roles (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  username varchar(20) NOT NULL,
-  role varchar(20) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY uni_username_role (role,username),
-  KEY fk_username_idx (username),
-  CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username)
-) engine=InnoDB;
+CREATE TABLE roles (
+  id              INTEGER AUTO_INCREMENT PRIMARY KEY,
+  username        VARCHAR(20) NOT NULL,
+  role            VARCHAR(20) NOT NULL
+);
+ALTER TABLE roles ADD CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username);
+CREATE INDEX fk_username_idx ON roles (username);
+
--- src/main/resources/db/mysql/populateDB.sql
+++ src/main/resources/db/mysql/populateDB.sql
@@ -1,59 +1,24 @@
-INSERT IGNORE INTO vets VALUES (1, 'James', 'Carter');
-INSERT IGNORE INTO vets VALUES (2, 'Helen', 'Leary');
-INSERT IGNORE INTO vets VALUES (3, 'Linda', 'Douglas');
-INSERT IGNORE INTO vets VALUES (4, 'Rafael', 'Ortega');
-INSERT IGNORE INTO vets VALUES (5, 'Henry', 'Stevens');
-INSERT IGNORE INTO vets VALUES (6, 'Sharon', 'Jenkins');
 
-INSERT IGNORE INTO specialties VALUES (1, 'radiology');
-INSERT IGNORE INTO specialties VALUES (2, 'surgery');
-INSERT IGNORE INTO specialties VALUES (3, 'dentistry');
+INSERT INTO dispense VALUES (1,'2016-06-22 19:10:25');
+INSERT INTO dispense VALUES (2,'2019-06-22 19:10:25');
+INSERT INTO dispense VALUES (3,'2019-08-15 15:48:19');
 
-INSERT IGNORE INTO vet_specialties VALUES (2, 1);
-INSERT IGNORE INTO vet_specialties VALUES (3, 2);
-INSERT IGNORE INTO vet_specialties VALUES (3, 3);
-INSERT IGNORE INTO vet_specialties VALUES (4, 2);
-INSERT IGNORE INTO vet_specialties VALUES (5, 1);
+INSERT INTO eye VALUES (1, '1', '1', '1','4');
+INSERT INTO eye VALUES (2, '1', '1', '1','3');
+INSERT INTO eye VALUES (3, '1', '1', '1','2');
+INSERT INTO eye VALUES (4, '1', '1', '1','1');
+INSERT INTO eye VALUES (5, '1', '1', '1','0');
+INSERT INTO eye VALUES (6, '1', '1', '1','-1');
 
-INSERT IGNORE INTO types VALUES (1, 'cat');
-INSERT IGNORE INTO types VALUES (2, 'dog');
-INSERT IGNORE INTO types VALUES (3, 'lizard');
-INSERT IGNORE INTO types VALUES (4, 'snake');
-INSERT IGNORE INTO types VALUES (5, 'bird');
-INSERT IGNORE INTO types VALUES (6, 'hamster');
+INSERT INTO glasses VALUES (1,4,'single','small','neutral',1,'San',FALSE,1,2);
 
-INSERT IGNORE INTO owners VALUES (1, 'George', 'Franklin', '110 W. Liberty St.', 'Madison', '6085551023');
-INSERT IGNORE INTO owners VALUES (2, 'Betty', 'Davis', '638 Cardinal Ave.', 'Sun Prairie', '6085551749');
-INSERT IGNORE INTO owners VALUES (3, 'Eduardo', 'Rodriquez', '2693 Commerce St.', 'McFarland', '6085558763');
-INSERT IGNORE INTO owners VALUES (4, 'Harold', 'Davis', '563 Friendly St.', 'Windsor', '6085553198');
-INSERT IGNORE INTO owners VALUES (5, 'Peter', 'McTavish', '2387 S. Fair Way', 'Madison', '6085552765');
-INSERT IGNORE INTO owners VALUES (6, 'Jean', 'Coleman', '105 N. Lake St.', 'Monona', '6085552654');
-INSERT IGNORE INTO owners VALUES (7, 'Jeff', 'Black', '1450 Oak Blvd.', 'Monona', '6085555387');
-INSERT IGNORE INTO owners VALUES (8, 'Maria', 'Escobito', '345 Maple St.', 'Madison', '6085557683');
-INSERT IGNORE INTO owners VALUES (9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', '6085559435');
-INSERT IGNORE INTO owners VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', '6085555487');
+INSERT INTO glasses VALUES (2,1,'single','small','neutral',2,'Halen',TRUE,3,4);
+INSERT INTO glasses VALUES (3,2,'single','small','neutral',3,'San',TRUE,5,6);
 
-INSERT IGNORE INTO pets VALUES (1, 'Leo', '2000-09-07', 1, 1);
-INSERT IGNORE INTO pets VALUES (2, 'Basil', '2002-08-06', 6, 2);
-INSERT IGNORE INTO pets VALUES (3, 'Rosy', '2001-04-17', 2, 3);
-INSERT IGNORE INTO pets VALUES (4, 'Jewel', '2000-03-07', 2, 3);
-INSERT IGNORE INTO pets VALUES (5, 'Iggy', '2000-11-30', 3, 4);
-INSERT IGNORE INTO pets VALUES (6, 'George', '2000-01-20', 4, 5);
-INSERT IGNORE INTO pets VALUES (7, 'Samantha', '1995-09-04', 1, 6);
-INSERT IGNORE INTO pets VALUES (8, 'Max', '1995-09-04', 1, 6);
-INSERT IGNORE INTO pets VALUES (9, 'Lucky', '1999-08-06', 5, 7);
-INSERT IGNORE INTO pets VALUES (10, 'Mulligan', '1997-02-24', 2, 8);
-INSERT IGNORE INTO pets VALUES (11, 'Freddy', '2000-03-09', 5, 9);
-INSERT IGNORE INTO pets VALUES (12, 'Lucky', '2000-06-24', 2, 10);
-INSERT IGNORE INTO pets VALUES (13, 'Sly', '2002-06-08', 1, 10);
 
-INSERT IGNORE INTO visits VALUES (1, 7, '2010-03-04', 'rabies shot');
-INSERT IGNORE INTO visits VALUES (2, 8, '2011-03-04', 'rabies shot');
-INSERT IGNORE INTO visits VALUES (3, 8, '2009-06-04', 'neutered');
-INSERT IGNORE INTO visits VALUES (4, 7, '2008-09-04', 'spayed');
 
-INSERT IGNORE INTO users(username,password,enabled) VALUES ('admin','{noop}admin', true);
+INSERT INTO users(username,password,enabled) VALUES ('admin','{noop}admin', true);
 
-INSERT IGNORE INTO roles (username, role) VALUES ('admin', 'ROLE_OWNER_ADMIN');
-INSERT IGNORE INTO roles (username, role) VALUES ('admin', 'ROLE_VET_ADMIN');
-INSERT IGNORE INTO roles (username, role) VALUES ('admin', 'ROLE_ADMIN');
+INSERT INTO roles (username, role) VALUES ('admin', 'ROLE_OWNER_ADMIN');
+INSERT INTO roles (username, role) VALUES ('admin', 'ROLE_VET_ADMIN');
+INSERT INTO roles (username, role) VALUES ('admin', 'ROLE_ADMIN');
