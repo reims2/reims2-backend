88b47afbf6d566361594f0b581d8a7eb7f28cdd3
Daniel <daniel-dall@outlook.com>
2022-09-02 16:05:34 +0200

    prevent sql injection with prepared statements

--- src/main/java/org/PVH/repository/jpa/CustomGlassesRepositoryImpl.java
+++ src/main/java/org/PVH/repository/jpa/CustomGlassesRepositoryImpl.java
@@ -1,11 +1,15 @@
 package org.PVH.repository.jpa;
 
 import java.math.BigInteger;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Date;
 
 import javax.persistence.EntityManager;
 import javax.persistence.LockModeType;
 import javax.persistence.NoResultException;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
 
 import org.PVH.model.Glasses;
@@ -22,18 +26,24 @@ public class CustomGlassesRepositoryImpl implements CustomGlassesRepository {
     @Transactional
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     public Glasses saveGlassesWithNextPossibleSKU(Glasses glasses, int min, int max) {
-        boolean minimumUsed = entityManager.createQuery("SELECT sku FROM Glasses WHERE sku = " + min).getResultList().size() > 0;
+        Query query = entityManager.createQuery("SELECT sku FROM Glasses WHERE sku = :min" );
+        query.setParameter("min",min);
+        boolean minimumUsed = query.getResultList().size() > 0;
+
 
         // If the smallest possible SKU is still free, just use it. This is important because otherwise this number will be left out
         Integer nextSKU = min;
 
         if (minimumUsed) {
             // otherwise find the next free SKU in the database
-            String query = "SELECT g.SKU + 1 AS FirstAvailableId FROM glasses g LEFT JOIN glasses g1 ON g1.SKU = g.SKU + 1 "
-                    + "WHERE g1.SKU IS NULL AND g.SKU >= " + min + " AND g.SKU < " + max + "  ORDER BY g.SKU LIMIT 0, 1";
+            Query findNextSKUQuery = entityManager.createNativeQuery("SELECT g.SKU + 1 AS FirstAvailableId FROM glasses g LEFT JOIN glasses g1 ON g1.SKU = g.SKU + 1 "
+                + "WHERE g1.SKU IS NULL AND g.SKU >= :min AND g.SKU < :max  ORDER BY g.SKU LIMIT 0, 1");
+            findNextSKUQuery.setParameter("min",min);
+            findNextSKUQuery.setParameter("max",max);
+
             try {
                 // JPA returns BigInteger even though it really is just a java int => convert it
-                nextSKU = ((BigInteger) entityManager.createNativeQuery(query).getSingleResult()).intValue();
+                nextSKU = ((BigInteger) findNextSKUQuery.getSingleResult()).intValue();
             } catch (NoResultException e) {
                 // all SKUs used, todo add some better error message in the future, for now
                 // throw exception
