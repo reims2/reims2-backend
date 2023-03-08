4642e033b72db7990a580205b459e04f0f705938
Thomas <substantialimpulse@pm.me>
2023-03-08 15:48:11 +0100

    add another reason

--- src/main/java/org/pvh/model/enums/DispenseReasonEnum.java
+++ src/main/java/org/pvh/model/enums/DispenseReasonEnum.java
@@ -1,5 +1,5 @@
 package org.pvh.model.enums;
 
 public enum DispenseReasonEnum {
-    DISPENSED, NOT_FOUND, BROKEN, TOO_HIGH_VALUES, OTHER // NOSONAR
+    DISPENSED, NOT_FOUND, BROKEN, TOO_HIGH_VALUES, WRONGLY_ADDED, OTHER // NOSONAR
 }
