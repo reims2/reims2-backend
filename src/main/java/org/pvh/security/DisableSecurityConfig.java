6e05352ac45605c24787ca4b717a8f23b9bc00e3
Thomas <substantialimpulse@pm.me>
2024-01-10 22:16:23 +0100

    chore: also update WebSecurity disable config

--- src/main/java/org/pvh/security/DisableSecurityConfig.java
+++ src/main/java/org/pvh/security/DisableSecurityConfig.java
@@ -7,17 +7,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
-/**
- * Starting from Spring Boot 2, if Spring Security is present, endpoints are secured by default using Spring Security’s
- * content-negotiation strategy.
- */
 @Configuration
 @ConditionalOnProperty(name = "pvh.security.enable", havingValue = "false")
 public class DisableSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
-                .and().authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+        http
+                .cors((cors) -> cors.configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()))
+                .csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().permitAll());
         return http.build();
     }
 
